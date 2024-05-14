503a1de7af31729543d690ffffcf5efc6a18aac6
rma-shuyu <1004129700@qq.com>
2024-05-14 11:32:49 +0800

    build rel

---
+++ build_rel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+CUR_PATH=`pwd`
+INSTALL_PATH=${CUR_PATH}/install_dir
+
+mkdir -p ${CUR_PATH}/install_dir
+
+cd qemu-8.0.5
+rm -rf build
+mkdir -p build
+cd build
+
+../configure --prefix=${INSTALL_PATH} --enable-kvm \
+--disable-debug-info --enable-vnc --target-list=x86_64-softmmu
+
+make -j8
+
+make install
+
+
